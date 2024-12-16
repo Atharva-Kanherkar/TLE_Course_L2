d1dc2b3e45d441805be0190fc93cbc0110e74a3f
icanpetyourcat <142440039+Atharva-Kanherkar@users.noreply.github.com>
2024-12-17 01:09:50 +0530

    Added notes

---
+++ Codes/Prefix_Sum/Subarray_Sums_II.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define vll vector<ll>
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define F first
+#define S second
+#define endl '\n'
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+
+void solve() {
+     int n , x;
+     cin >> n >> x;
+        vll a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+        vll prefix(n);
+        prefix[0] = a[0];
+        for (int i = 1; i < n; i++) {   
+            prefix[i] = prefix[i - 1] + a[i];
+        }
+        ll ans = 0;
+        map<ll, ll> cnt;
+        for (int i = 0; i < n; i++) {
+            if (prefix[i] == x) ans++;
+             if (cnt.find(prefix[i] - x) != cnt.end()) {
+    ans += cnt[prefix[i] - x];
+}
+            cnt[prefix[i]]++;
+        }
+        cout<<ans<<endl;
+}
+
+int main() {
+    fastio;
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
